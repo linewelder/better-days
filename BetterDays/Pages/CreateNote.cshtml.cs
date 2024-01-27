6aea2efc402db5d63c52f10e99f3d3b281751527
linewelder <redalerm@gmail.com>
2024-01-27 22:41:26 +0100

    Move note creation form to separate page

---
+++ BetterDays/Pages/CreateNote.cshtml.cs
@@ -0,0 +1,91 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using BetterDays.Data;
+using BetterDays.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BetterDays.Pages;
+
+public class CreateNote(ApplicationDbContext context) : PageModel
+{
+    public class NewDailyNote
+    {
+        [Required]
+        public DateOnly Date { get; init; }
+
+        public string? Comment { get; init; }
+
+        [DisplayName("Deeds Done")]
+        public List<int>? DoneDeedIds { get; init; }
+    }
+
+    [BindProperty]
+    public NewDailyNote NewNote { get; set; } = null!;
+
+    public List<Deed> Deeds { get; set; } = null!;
+
+    public async Task PopulatePage()
+    {
+        NewNote = new NewDailyNote
+        {
+            Date = DateOnly.FromDateTime(DateTime.Today)
+        };
+        Deeds = await context.Deeds.ToListAsync();
+    }
+
+    public async Task<IActionResult> OnGet()
+    {
+        if (!User.Identity?.IsAuthenticated ?? false)
+        {
+            return Redirect("/Identity/Account/Login");
+        }
+
+        await PopulatePage();
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (!User.Identity?.IsAuthenticated ?? false)
+        {
+            return Redirect("/Identity/Account/Login");
+        }
+
+        var date = NewNote.Date;
+        if (await context.DailyNotes.AnyAsync(dn => dn.Date == date))
+        {
+            ModelState.AddModelError("", "There already is a note for today");
+        }
+
+        if (!ModelState.IsValid)
+        {
+            await PopulatePage();
+            return Page();
+        }
+
+        var newNote = new DailyNote
+        {
+            Date = date,
+            Comment = NewNote.Comment?.Trim(),
+            Deeds = NewNote.DoneDeedIds?
+                .Select(id => new DoneDeed
+                {
+                    DailyNoteDate = date,
+                    DeedId = id
+                })
+                .ToList()
+        };
+        if (!TryValidateModel(newNote))
+        {
+            await PopulatePage();
+            return Page();
+        }
+
+        context.Add(newNote);
+        await context.SaveChangesAsync();
+
+        return RedirectToPage(nameof(Index));
+    }
+}
--- BetterDays/Pages/Index.cshtml.cs
+++ BetterDays/Pages/Index.cshtml.cs
@@ -1,7 +1,4 @@
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using BetterDays.Data;
-using BetterDays.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,17 +7,6 @@ namespace BetterDays.Pages;
 
 public class Index(ApplicationDbContext context) : PageModel
 {
-    public class NewDailyNote
-    {
-        [Required]
-        public DateOnly Date { get; init; }
-
-        public string? Comment { get; init; }
-
-        [DisplayName("Deeds Done")]
-        public List<int>? DoneDeedIds { get; init; }
-    }
-
     public class HistoryItem
     {
         public DateOnly Date { get; init; }
@@ -28,21 +14,10 @@ public class Index(ApplicationDbContext context) : PageModel
         public required List<string> DoneDeeds { get; init; }
     }
 
-    [BindProperty]
-    public NewDailyNote NewNote { get; set; } = null!;
-
-    public List<Deed> Deeds { get; set; } = null!;
-
     public List<HistoryItem> Items { get; private set; } = null!;
 
     private async Task PopulatePage()
     {
-        NewNote = new NewDailyNote
-        {
-            Date = DateOnly.FromDateTime(DateTime.Today)
-        };
-        Deeds = await context.Deeds.ToListAsync();
-
         Items = await context.DailyNotes
             .Include(day => day.Deeds!)
             .ThenInclude(dd => dd.Deed)
@@ -62,50 +37,6 @@ public class Index(ApplicationDbContext context) : PageModel
         {
             return Redirect("/Identity/Account/Login");
         }
-
-        await PopulatePage();
-        return Page();
-    }
-
-    public async Task<IActionResult> OnPost()
-    {
-        if (!User.Identity?.IsAuthenticated ?? false)
-        {
-            return Redirect("/Identity/Account/Login");
-        }
-
-        var date = NewNote.Date;
-        if (await context.DailyNotes.AnyAsync(dn => dn.Date == date))
-        {
-            ModelState.AddModelError("", "There already is a note for today");
-        }
-
-        if (!ModelState.IsValid)
-        {
-            await PopulatePage();
-            return Page();
-        }
-
-        var newNote = new DailyNote
-        {
-            Date = date,
-            Comment = NewNote.Comment?.Trim(),
-            Deeds = NewNote.DoneDeedIds?
-                .Select(id => new DoneDeed
-                {
-                    DailyNoteDate = date,
-                    DeedId = id
-                })
-                .ToList()
-        };
-        if (!TryValidateModel(newNote))
-        {
-            await PopulatePage();
-            return Page();
-        }
-
-        context.Add(newNote);
-        await context.SaveChangesAsync();
 
         await PopulatePage();
         return Page();
