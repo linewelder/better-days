ce0ca8730252e3e03d6e94fb0983e71482528ec3
linewelder <redalerm@gmail.com>
2024-01-27 04:28:04 +0100

    Create AddModels migration

--- BetterDays/Data/Migrations/20240127032721_AddModels.Designer.cs
+++ BetterDays/Data/Migrations/20240127032721_AddModels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BetterDays.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BetterDays.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240127032721_AddModels")]
+    partial class AddModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
