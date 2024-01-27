e6640ad5d0dc513162b1a4b8b8667bc2db476570
linewelder <redalerm@gmail.com>
2024-01-27 03:06:47 +0100

    Add simple frontend

--- BetterDays/Pages/Index.cshtml.cs
+++ BetterDays/Pages/Index.cshtml.cs
@@ -1,19 +1,41 @@
+using BetterDays.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BetterDays.Pages;
 
-public class IndexModel : PageModel
+public class Index(ApplicationDbContext context) : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class HistoryItem
     {
-        _logger = logger;
+        public DateOnly Date { get; init; }
+        public string? Comment { get; init; }
+        public required List<string> DoneDeeds { get; init; }
     }
 
-    public void OnGet()
-    {
+    public List<HistoryItem> Items = null!;
+    public List<HistoryItem> Items { get; private set; } = null!;
 
+    public async Task<IActionResult> OnGet()
+    {
+        if (!User.Identity?.IsAuthenticated ?? false)
+        {
+            return Redirect("/Identity/Account/Login");
+        }
+
+        Items = await context.DailyNotes
+            .Include(day => day.Deeds!)
+            .ThenInclude(dd => dd.Deed)
+            .Select(day => new HistoryItem
+            {
+                Date = day.Date,
+                Comment = day.Comment,
+                DoneDeeds = day.Deeds!.Select(dd => dd.Deed!.Name).ToList()
+            })
+            .OrderByDescending(day => day.Date)
+            .ToListAsync();
+
+        return Page();
     }
 }
