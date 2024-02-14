9cfdcbe0c88a06e84432baaff726f8cb30b3478d
linewelder <redalerm@gmail.com>
2024-02-14 14:06:44 +0100

    fix: Calculate perceived date on client side
    Client and server might be in different time zones.

--- BetterDays/wwwroot/js/site.js
+++ BetterDays/wwwroot/js/site.js
@@ -1,4 +1,54 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/**
+ * The hour at which the next day starts.
+ */
+const perceivedDayBoundary = 6;
 
-// Write your JavaScript code.
+/**
+ * If it is after midnight, the note is most likely to be
+ * about the day that has just ended.
+ */
+function getPerceivedToday() {
+    const today = new Date();
+    const stillNotSleeping = today.getHours() < perceivedDayBoundary;
+    if (stillNotSleeping) {
+        today.setDate(today.getDate() - 1);
+    }
+
+    return today;
+}
+
+const daysOfWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+];
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+/**
+ * Formats the date in the human readable format ("dddd, d mmm yyyy" in C#).
+ */
+function formatDate(date) {
+    const dayOfWeek = daysOfWeek[date.getDay()];
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    return `${dayOfWeek}, ${day} ${month} ${year}`;
+}
